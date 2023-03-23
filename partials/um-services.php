cc6d81a30b583179eeedac16c1836ab999749b5a
webozza <webozza@gmail.com>
2023-03-23 10:31:40 +0800

    version update

--- partials/um-services.php
+++ partials/um-services.php
@@ -6,34 +6,23 @@
     $service = new WP_Query($query_services);
 ?>
     
-    
+<!-- RENDER SERVICES LOOP -->
 <div class ="services-container">
-    <div class="um-prev-service swiper-button-prev"></div>
-    <div class="swiper">
-        <div class="swiper-wrapper">
-            <?php if ( $service->have_posts() ) : ?>
+    <?php if ( $service->have_posts() ) : ?>
+        <?php while ( $service->have_posts() ) : $service->the_post(); ?>
 
-                <?php while ( $service->have_posts() ) : $service->the_post(); ?>
-
-                    <div class="swiper-slide">
-                        <a href="<?= get_permalink() ?>">
-                            <div class="service-box">
-                                <div class="service-thumb">
-                                    <?php the_post_thumbnail() ?>
-                                </div>
-                                <div class="service-title">
-                                    <h5><?php the_title(); ?></h5>
-                                </div>
-                            </div>
-                        </a>
+            <a href="<?= get_permalink() ?>">
+                <div class="service-box">
+                    <div class="service-thumb">
+                        <?php the_post_thumbnail() ?>
                     </div>
-                        
-                <?php endwhile; ?>
-                <?php wp_reset_postdata(); ?>
-            <?php endif; ?>
-        </div>
-        <div class="swiper-button-prev hidden"></div>
-        <div class="swiper-button-next hidden"></div>
-    </div>
-    <div class="um-next-service swiper-button-next"></div>
+                    <div class="service-title">
+                        <h5><?php the_title(); ?></h5>
+                    </div>
+                </div>
+            </a>
+                
+        <?php endwhile; ?>
+        <?php wp_reset_postdata(); ?>
+    <?php endif; ?>
 </div>
