92c81909ca2b5be314a1541f60a8a77c84ddffc4
webozza <webozza@gmail.com>
2023-03-23 12:10:27 +0800

    version update

--- partials/um-services.php
+++ partials/um-services.php
@@ -9,7 +9,7 @@
 <!-- RENDER SERVICES LOOP -->
 <div class ="services-container">
     <?php if ( $service->have_posts() ) : ?>
-        <?php $i = 1; while ( $service->have_posts() && $i < 10) : $service->the_post(); ?>
+        <?php while ( $service->have_posts() ) : $service->the_post(); ?>
 
             <a href="<?= get_permalink() ?>">
                 <div class="service-box">
@@ -22,7 +22,8 @@
                 </div>
             </a>
                 
-        <?php $i++; endwhile; ?>
+        <?php endwhile; ?>
         <?php wp_reset_postdata(); ?>
     <?php endif; ?>
 </div>
+<a class="um-naked-btn" href="">Explore Industries</a>
