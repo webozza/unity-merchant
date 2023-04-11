69f1a264ccb3797e7c021d3deba94e65fbd2bbc9
webozza <webozza@gmail.com>
2023-04-11 09:38:12 +0800

    um_industries shortcode updated

--- partials/um-industries.php
+++ partials/um-industries.php
@@ -1,1 +1,7 @@
-<h1>fijsdifjsidjfidsj</h1>
+<div class="um-industries">
+    <div class="um-left-col">
+        <h2>Specialized Industries</h2>
+        <p>Empowering Specialized Industries with Tailored Solutions</p>
+    </div>
+    <div class="um-right-col"></div>
+</div>
