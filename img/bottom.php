10e5b16b3b0370bc17234bcf4f4511a955bb790d
webozza <webozza@gmail.com>
2023-04-12 11:28:09 +0800

    version update

---
+++ img/bottom.php
@@ -0,0 +1,19 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1400 600" width="1400" height="600" preserveAspectRatio="xMidYMid slice" style="width: 100%; height: 100%; transform: translate3d(0px, 0px, 0px); content-visibility: visible;">
+  <defs>
+    <clipPath id="__lottie_element_25">
+      <rect width="1400" height="600" x="0" y="0"></rect>
+    </clipPath>
+    <clipPath id="__lottie_element_27">
+      <path d="M0,0 L1400,0 L1400,600 L0,600z"></path>
+    </clipPath>
+  </defs>
+  <g clip-path="url(#__lottie_element_25)">
+    <g clip-path="url(#__lottie_element_27)" transform="matrix(-1,0,0,-1,1400,600)" opacity="1" style="display: block;">
+      <g transform="matrix(1,0,0,1,0,0)" opacity="1" style="display: block;">
+        <g opacity="1" transform="matrix(1,0,0,1,0,0)">
+          <path fill="#3674f9" fill-opacity="1" d=" M732.2620239257812,393.63299560546875 C732.2620239257812,393.63299560546875 699.9450073242188,600 699.9450073242188,600 C699.9450073242188,600 1400.0570068359375,600 1400.0570068359375,600 C1400.0570068359375,600 1400.0570068359375,356.1109924316406 1400.0570068359375,356.1109924316406 C1400.0570068359375,356.1109924316406 776.1290283203125,356.1109924316406 776.1290283203125,356.1109924316406 C754.2730102539062,356.12701416015625 735.6669921875,372.0379943847656 732.2620239257812,393.63299560546875z"></path>
+        </g>
+      </g>
+    </g>
+  </g>
+</svg>
--- partials/um-industries.php
+++ partials/um-industries.php
@@ -14,3 +14,20 @@
         </div>
     </div>
 </div>
+<div class="um-industries-2">
+    <div class="um-left-col">
+        <div class="um-content-wrapper">
+            <h2>Specialized Industries</h2>
+            <p>Empowering Specialized Industries with Tailored Solutions</p>
+        </div>
+    </div>
+    <div class="um-right-col">
+        <div class="um-svg">
+            <?php include( get_stylesheet_directory() . '/img/bottom.php' ) ?>
+        </div>
+        <div class="um-svg-img">
+            <h3>More places to shop</h3>
+            <h3>More places to make your money work harder</h3>
+        </div>
+    </div>
+</div>
