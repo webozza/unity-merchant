ec514044328b57f56ff906781a3812a39dd22f22
webozza <webozza@gmail.com>
2023-04-12 09:45:05 +0800

    version update

--- partials/um-industries.php
+++ partials/um-industries.php
@@ -1,10 +1,16 @@
 <div class="um-industries">
     <div class="um-left-col">
-        <h2>Specialized Industries</h2>
-        <p>Empowering Specialized Industries with Tailored Solutions</p>
+        <div class="um-content-wrapper">
+            <h2>Specialized Industries</h2>
+            <p>Empowering Specialized Industries with Tailored Solutions</p>
+        </div>
     </div>
     <div class="um-right-col">
-        <?php include( get_stylesheet_directory() . '/img/top.php' ) ?>
-        <img src="<?= get_stylesheet_directory_uri() . '/img/um-industries.jpg' ?>">
+        <div class="um-svg">
+            <?php include( get_stylesheet_directory() . '/img/top.php' ) ?>
+        </div>
+        <div class="um-svg-img">
+            <img src="<?= get_stylesheet_directory_uri() . '/img/um-industries.jpg' ?>">
+        </div>
     </div>
 </div>
